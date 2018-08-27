d2c77f64cc79e10144cf6595cde840adcf02b7bb
James Birmingham <jbirmingham40@gmail.com>
2018-08-27 15:15:13 -0500

    Test 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         var str = "test";
-        System.out.println("Hello world!" + " " + str);
+        System.out.println("Hello world!" + " " + str "...");
     }
 }
